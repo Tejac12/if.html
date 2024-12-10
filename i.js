f7ff00f1bf892effdfe1c164078c66110c441d5e
Tejac12 <chintalatejavardhan692@gmail.com>
2024-12-10 16:04:10 +0530

    Add files via upload

---
+++ i.js
@@ -0,0 +1,45 @@
+//Your age is 22, if your age is greater than 18, console the “eligible for vote”, otherwise console  “not eligible”
+
+let age = 22;
+
+if (age > 18) {
+    console.log("eligible for vote")
+} else {
+    console.log("not eligible to vote")
+}
+
+
+
+
+//Weather condition is “rainy”,if weather condition is rainy, console the “its pouring outside” otherwise “not raining”
+
+let weather = "rainy";
+
+if (weather = "rainy") {
+    console.log("its pouring outside")
+} else {
+    console.log("not raining")
+}
+
+
+
+//Students marks is 76, console the grade of students based on marks;
+
+let marks = 76;
+
+if (marks > 90) {
+    console.log("the grade should be A+")
+} else if (marks >= 81) {
+    console.log("the grade should be A")
+}
+else if (marks >= 71) {
+    console.log("the grade should be B+")
+} else if (marks >= 61) {
+    console.log("the grade should be C+")
+} else if (marks >= 51) {
+    console.log("the grade should be C")
+} else if (marks >= 40) {
+    console.log("the grade should be D")
+} else if (marks < 50) {
+    console.log("fail")
+}
